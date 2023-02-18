153b91121f6f1454b1b407ded86a1e3ac0b3e189
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 02:55:18 +0900

    BuildContext: 먼 부모 스타일 가져오기

--- lib/main.dart
+++ lib/main.dart
@@ -4,58 +4,66 @@ void main() {
   runApp(App());
 }
 
-// stateless widget은 단지 UI를 리턴해주는 역할만을 한다.
-
-/* stateful widget은 state(상태)를 가진 위젯을 말한다.(실시간 데이터 반영)
-statefulWidget은 두가지 파트로 나뉘는데 하나는 상태가 없는 위젯
-하나는 State 위젯은 위젯에 들어갈 데이터와 UI를 넣는 곳.
-
-*/
-
-//state가 없는 위젯 자체
 class App extends StatefulWidget {
   @override
   State<App> createState() => _AppState();
 }
 
-//State 위젯은 데이터가 반영되면서 UI가 변화하는 곳.
 class _AppState extends State<App> {
-  // 아래 값을 변경하고 덮어쓸 예정이라 final을 붙이지 않는다.
-  // 또한 데이터는 단지 dart 클래스의 프로퍼티
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
-    //react의 useState처럼 상태 변경을 알려서 렌더링(build메서드 호출)을 발생시키는 setState함수가 필요
-    setState(() => {});
-    // 콜백 내에 아래 표현식을 넣어도 되고 걍 밖에 둬도 작동함.
-    counter = counter + 1;
+    setState(() => {numbers.add(numbers.length)});
   }
 
-// State 클래스는 위젯의 UI도 보유
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //flutter는 앱의 모든 스타일을 한 곳에 저장할 수 있는 기능을 제공
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              '$counter',
-              style: const TextStyle(fontSize: 30),
-            ),
-            IconButton(
-              iconSize: 40,
-              onPressed: onClicked,
-              icon: const Icon(Icons.add_box_rounded),
-            ),
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MyLargeTitle(),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 자식 클래스는 BuildContext를 통해 부모 위젯의 theme 값에 접근 가능
+    // 사실상 MyLargeTitle 위젯은 column -> Center -> Scaffold를 지나서야(위젯트리참고)
+    // Material 위젯에 도달할 수 있음.
+    //context는 Text 위에 있는 모든 상위 요소들에 대한 정보.
+    //즉, context에는 위젯 트리에 대한 정보가 담겨있음.
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        //Theme.of(context) : 위젯의 현재 위치를 제공
+        //ThemeData에 저장된 textTheme의 titleLarge의 color 값을 가져옴
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
+    );
+  }
+}
+
+// BuildContext는 위젯 트리에서 위젯의 위치를 제공하고
+// 이를 통해 상위 요소 데이터에 접근 가능하다.
